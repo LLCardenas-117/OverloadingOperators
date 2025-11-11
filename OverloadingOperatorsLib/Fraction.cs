ea5d0b41800387b87111daa7e35316af8fee5a56
LLCardenas-117 <lcardenas@student.neumont.edu>
2025-11-10 19:10:47 -0700

    Add project files.

---
+++ OverloadingOperatorsLib/Fraction.cs
@@ -0,0 +1,205 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace OverloadingOperatorsLib
+{
+    public struct Fraction
+    {
+        public int WholeNumber { get; set; }
+        public int Numerator { get; set; }
+
+        private int denominator = 1;
+
+        public int Denominator
+        {
+            get { return denominator; }
+            set {
+                if (value == 0) throw new ArgumentException("The denominator can never be 0.");
+                denominator = value;
+            }
+        }
+
+        public Fraction()
+        {
+            Denominator = 1;
+        }
+
+        public Fraction(int whole = 0, int numerator = 0, int denominator = 1)
+        {
+            WholeNumber = whole;
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public static int GCD(int m, int n) {
+            if (m == 0) return n;
+            else return GCD(n % m, m);
+        }
+
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            // Make both fractions improper
+            f1.MakeImproper();
+            f2.MakeImproper();
+
+            // Create new fraction
+            Fraction f3 = new Fraction();
+
+            // Multiply both denoms to get and store the common denom
+            // set result denom to common denom
+            f3.Denominator = f1.Denominator * f2.Denominator;
+
+            // Multiply each num by it's opposite denom
+            f1.Numerator *= f2.Denominator;
+            f2.Numerator *= f1.Denominator;
+
+            // Add the new nums to get the result num
+            f3.Numerator = f1.Numerator + f2.Numerator;
+
+            // simplify result and return
+            f3.Simplify();
+
+            return f3;
+        }
+
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            // Make both fractions improper
+            f1.MakeImproper();
+            f2.MakeImproper();
+
+            // Create new fraction
+            Fraction f3 = new Fraction();
+
+            // Multiply both denoms to get and store the common denom
+            // set result denom to common denom
+            f3.Denominator = f1.Denominator * f2.Denominator;
+
+            // Multiply each num by it's opposite denom
+            f1.Numerator *= f2.Denominator;
+            f2.Numerator *= f1.Denominator;
+
+            // Add the new nums to get the result num
+            f3.Numerator = f1.Numerator - f2.Numerator;
+
+            // simplify result and return
+            f3.Simplify();
+
+            return f3;
+        }
+
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            // Make both fractions improper
+            f1.MakeImproper();
+            f2.MakeImproper();
+
+            // Create new fraction
+            Fraction f3 = new Fraction();
+
+            // Multiply both denoms to get and store the common denom
+            // set result denom to common denom
+            f3.Denominator = f1.Denominator * f2.Denominator;
+
+            // Multiply each num by it's opposite denom
+            f1.Numerator *= f2.Denominator;
+            f2.Numerator *= f1.Denominator;
+
+            // Add the new nums to get the result num
+            f3.Numerator = f1.Numerator * f2.Numerator;
+
+            // simplify result and return
+            f3.Simplify();
+
+            return f3;
+        }
+
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            // Make both fractions improper
+            f1.MakeImproper();
+            f2.MakeImproper();
+
+            // Create new fraction
+            Fraction f3 = new Fraction();
+
+            // Multiply both denoms to get and store the common denom
+            // set result denom to common denom
+            f3.Denominator = f1.Denominator * f2.Denominator;
+
+            // Multiply each num by it's opposite denom
+            f1.Numerator *= f2.Denominator;
+            f2.Numerator *= f1.Denominator;
+
+            // Add the new nums to get the result num
+            f3.Numerator = f1.Numerator / f2.Numerator;
+
+            // simplify result and return
+            f3.Simplify();
+
+            return f3;
+        }
+
+
+        public static bool operator ==(Fraction f1, Fraction f2)
+        {
+            // make improper
+            f1.MakeImproper();
+            f2.MakeImproper();
+            // reduce both
+            f1.Reduce();
+            f2.Reduce();
+            // check if num and denom are equal to eachother
+            if ((f1.Numerator == f2.Numerator) && (f1.Denominator == f2.Denominator))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+        
+        public static bool operator !=(Fraction f1, Fraction f2)
+        {
+            return !(f1 == f2);
+        }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (obj is Fraction other)
+            {
+                return this == other;
+            }
+            return false;
+        }
+
+        // Make proper
+        public void MakeProper()
+        {
+            // divide num by denom using int set to whole
+            WholeNumber += Numerator / Denominator;
+            // mod num by denom using int set to num
+            Numerator %= Denominator;
+        }
+
+        // divide the num and denom by the gcd
+        public void Reduce()
+        {
+            int commonDivisor = GCD(Numerator, Denominator);
+            Numerator /= commonDivisor;
+            Denominator /= commonDivisor;
+        }
+
+        // Simplify (just calls reduce and make proper)
+        public void Simplify()
+        {
+            Reduce();
+            MakeProper();
+        }
+
+        private void MakeImproper()
+        {
+            Numerator = (WholeNumber * Denominator) + Numerator;
+            WholeNumber = 0;
+        }
+    }
+}
---
+++ OverloadingOperatorsLibTester/UnitTest1.cs
@@ -0,0 +1,123 @@
+﻿using OverloadingOperatorsLib;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Tracing;
+using Xunit.Sdk;
+
+namespace OverloadingOperatorsLibTester
+{
+    public class FractionTests
+    {
+        //There are 4 primary rules to automated testing
+        // 1. Every test should think it's the only test
+        // 2. Every test should start from a place of newness
+        // 3. Every test should test one thing
+        // 4. Test EVERYTHING
+
+        [Fact]
+        public void GCD_5and3_Returns1()
+        {
+            // There are 3 stages to every test
+            // Arrange
+            int num1 = 5;
+            int num2 = 3;
+            int expected = 1;
+            // Act
+            int actual = Fraction.GCD(num1, num2);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        //[Fact]
+        //public void GCD_15and3_Returns3()
+        //{
+        //    // There are 3 stages to every test
+        //    // Arrange
+        //    int num1 = 15;
+        //    int num2 = 3;
+        //    int expected = 3;
+        //    // Act
+        //    int actual = Fraction.GCD(num1, num2);
+        //    // Assert
+        //    Assert.Equal(expected, actual);
+        //}
+
+        //[Fact]
+        //public void GCD_3and15_Returns3()
+        //{
+        //    // There are 3 stages to every test
+        //    // Arrange
+        //    int num1 = 3;
+        //    int num2 = 15;
+        //    int expected = 3;
+        //    // Act
+        //    int actual = Fraction.GCD(num1, num2);
+        //    // Assert
+        //    Assert.Equal(expected, actual);
+        //}
+
+        [Theory]
+        [InlineData(15, 3, 3)]
+        [InlineData(3, 15, 3)]
+        [InlineData(30, 40, 10)]
+        [InlineData(35, 35, 35)]
+        [InlineData(5, 3, 1)]
+        public void GCD_ReturnsCorrectValue(int m, int n, int expected)
+        {
+            // There are 3 stages to every test
+            // Arrange
+            // Act
+            int actual = Fraction.GCD(m, n);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(0, 1, 2, 0, 10, 15, 1, 1, 6)]
+        [InlineData(0, 1, 2, 0, 10, 15, 1, 1, 3)]
+        //[InlineData(0, 20, 5, 0, 20, 5, 8)]
+        //[TestCaseOrderer(new Fraction(0, 15, 3), new Fraction(0, 15, 3), new Fraction(10, 0, 3))]
+        //[InlineData(, , )]
+        public void Fraction_ReturnCorrectAdditionValue(int w1, int n1, int d1, int w2, int n2, int d2, int w3, int n3, int d3)
+        {
+            Fraction f1 = new Fraction(w1, n1, d1);
+            Fraction f2 = new Fraction(w2, n2, d2);
+            Fraction actual = f1 + f2;
+            Fraction expected = new Fraction(w3, n3, d3);
+            //int actual = Fraction.GCD(f3.Numerator, f3.Denominator);
+
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Theory]
+        [InlineData(0, 1, 2,  0, 1, 4,  0, 1, 4)]
+        [InlineData(0, 1, 2,  0, 1, 4,  0, 1, 2)]
+        public void Fraction_ReturnCorrectSubtractionValue(int w1, int n1, int d1, int w2, int n2, int d2, int w3, int n3, int d3)
+        {
+            Fraction f1 = new Fraction(w1, n1, d1);
+            Fraction f2 = new Fraction(w2, n2, d2);
+            Fraction actual = f1 - f2;
+            Fraction expected = new Fraction(w3, n3, d3);
+            //int actual = Fraction.GCD(f3.Numerator, f3.Denominator);
+
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Theory]
+        [InlineData(0, 1, 2, 0, 1, 4, 0, 1, 8)]
+        //[InlineData(0, 1, 2, 0, 1, 4, 0, 1, 2)]
+        public void Fraction_ReturnCorrectMultiplicationValue(int w1, int n1, int d1, int w2, int n2, int d2, int w3, int n3, int d3)
+        {
+            Fraction f1 = new Fraction(w1, n1, d1);
+            Fraction f2 = new Fraction(w2, n2, d2);
+            Fraction actual = f1 * f2;
+            Fraction expected = new Fraction(w3, n3, d3);
+            //int actual = Fraction.GCD(f3.Numerator, f3.Denominator);
+
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
